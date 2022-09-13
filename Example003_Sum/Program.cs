bb3b16b1cccd6fcf5f9ecfcbe1ea6900ebae4097
Anton Savenkov <a_savi@icloud.com>
2022-09-13 22:15:47 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3321;
-int numberB = 9;
+﻿int numberA = new Random().Next(3,59);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,9);
+Console.WriteLine(numberB);
 int resuult = (numberA + numberB);
 Console.WriteLine(resuult);
